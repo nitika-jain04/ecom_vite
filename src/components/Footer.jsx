bd552f58d4f291168305554bd63b9f5311f518cb
Nitika Jain <nitikaaajain.04@gma.com>
2024-08-05 17:11:29 +0530

    commits

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { IoCall } from "react-icons/io5";
 
 const Footer = () => {
   return (
-    <div className="bg-red-400 px-5 py-10 mt-20 flex items-center justify-between font-semibold text-lg">
+    <div className="bg-red-400 px-5 py-10 lg:px-40 lg:text-lg mt-20 flex items-center justify-between font-semibold text-lg">
       <div className="flex flex-col gap-10">
         <ul>
           <li>Contact Us</li>
